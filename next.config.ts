0af98fe828901769f1cbb89ff8187e2a790229e7
Kangyun Wang <me@kyunwang.nl>
2024-12-13 15:48:02 +0100

    Set packages an linting

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,45 @@
+import bundleAnalyzer from "@next/bundle-analyzer";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  experimental: {
+    turbo: {
+      rules: {
+        "*.svg": {
+          loaders: ["@svgr/webpack"],
+          as: "*.js",
+        },
+      },
+    },
+    nextScriptWorkers: true,
+    reactCompiler: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  redirects: async () => [
+    {
+      source: "/home",
+      destination: "/",
+      permanent: true,
+    },
+  ],
+  rewrites: async () => [
+    {
+      source: "/",
+      destination: "/home",
+    },
+  ],
 };
 
-export default nextConfig;
+const bundleAnalyzerPlugin = bundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
+
+const NextApp = () => {
+  const plugins = [bundleAnalyzerPlugin];
+  return plugins.reduce((config, plugin) => plugin(config), nextConfig);
+};
+
+export default NextApp;
